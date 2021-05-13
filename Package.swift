74e758f5828ec9f669c9176ad2415fa8e51cddc2
ivol <ivol@criticalblue.com>
2021-05-13 13:59:37 +0100

    Initial changes

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Approov",
+    platforms: [.iOS(.v10)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Approov",
+            targets: ["Approov"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "Approov",
+            url: "https://github.com/ivolz/swift-pm/releases/download/2.6.1/Approov.xcframework.zip",
+            checksum: "36f93031289a4d88b9a1bda98118c7c80a1b764aea4b0e0c1f8bbc291d12fbc1"
+            ),
+    ]
+)
+
+
